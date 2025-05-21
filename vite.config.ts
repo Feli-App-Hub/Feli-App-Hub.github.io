93bfb89c9a3ea18cae9ff9ba826e6fb452d188d5
Felici4no <lucas.for.study.42@gmail.com>
2025-05-21 19:42:15 -0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Como o repositório é Feli-App-Hub.github.io, o site roda na raiz:
+  base: '/', 
+
+  // Se você não estiver tendo problemas com o lucide-react, pode remover este exclude.
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
